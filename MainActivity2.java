ced9269d41519d26a3f08ec1d4560e3851d09566
pingu11 <katja.hachenberg@gmx.net>
2014-11-04 13:01:46 +0100

    Update MainActivity2.java
    updated

--- MainActivity2.java
+++ MainActivity2.java
@@ -13,21 +13,14 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+
 import android.widget.Toast;
 import android.widget.VideoView;
  
 public class MainActivity extends Activity {
  
     // Activity request codes
-    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
-    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
+
  
     // directory name to store captured images and videos
 
